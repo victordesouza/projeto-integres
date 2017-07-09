2685e564b2564193c8155e976fd66e9c45c0da8b
Unknown <victordesouzaabreu@gmail.com>
2017-07-09 22:25:45 +0000

    Segurança Login e Banco 09/07/17

--- victor/verificaLogin.php
+++ victor/verificaLogin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include("ConectaBanco.php");
 $cont= 0;
 $nome = $_POST['nome'];
@@ -6,31 +6,30 @@ $nome = $_POST['nome'];
 $password = $_POST['password'];
 $SenhaArray = array();
 
+$nome = mysqli_real_escape_string($conexao, $nome);
+$passMd5 = md5($password);
+
 if ($nome == NULL || $password == NULL){
 	header('Location: login.php?error=true');
 }
 
+$qySenha = mysqli_query($conexao,"select p.SENHA,p.USER,q.TIPO from login p
+				left outer join cad_empresa q on (q.cod = p.COD_EMPRESA)
+				where upper(p.USER) = upper('$nome') and p.SENHA = '$passMd5'");
 
-$qySenha = mysqli_query($conexao,"select p.SENHA,p.USER,q.TIPO from login p left outer join cad_empresa q on (q.cod = p.COD_EMPRESA) where upper(p.USER) = upper('$nome') and p.SENHA <> 'NULL'"); 	
+$login = mysqli_fetch_assoc($qySenha);
 
-while($SenhaArray = mysqli_fetch_assoc($qySenha)) {
-	$cont ++;
-	if($password == $SenhaArray["SENHA"]){
-		if ($SenhaArray["TIPO"] == 1) {
-			header('Location: industriaInicial.php',TRUE,307);
-			$_SESSION['tipo'] = 1;
-		}else {
-			header('Location: comercioInicial.php',TRUE,307);
-			$_SESSION['tipo'] = 2;
-		} 
+if ($login == NULL) {
+	header('Location: login.php?error=true');
+}else{
+	if ($login["TIPO"] == 1) {
+		header('Location: industriaInicial.php',TRUE,307);
+		$_SESSION['tipo'] = 1;
 	}else {
-		header('Location: login.php?error=true');
-	}		
+		header('Location: comercioInicial.php',TRUE,307);
+		$_SESSION['tipo'] = 2;
+	}
 }
 
-if ($cont == 0) {
-
-header('Location: login.php?error=true');
-}
 include("rodape.php");
 ?>
